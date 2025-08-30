e83142a809ccebaeae8c097c9dcbb730a65f07fc
reymonsta <rascalrey15@gmail.com>
2025-08-31 01:53:40 +0800

    PythonFiles as normal folder instead of submodule

---
+++ PythonFiles/Demo.py
@@ -0,0 +1,27 @@
+# #Typecasting int() str() float() bool()
+#
+# name = "John Reymar"
+# age = 25
+# gpa = 3.2
+# is_student = True
+#
+#
+# name = bool(name)
+#
+# print(name)
+
+
+#input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+# name = input("What is your name?: ")
+# age = int(input("How old are you?: "))
+#
+#
+# age = age + 1
+#
+# print(f"Hello {name}!")
+# print("HAPPY BIRTHDAY!")
+# print(f"You are {age} years old!")
+
+
+
